0fc910f138957237e873026f59b385f2856bf867
Carli <carlibuckle@gmail.com>
2022-03-11 22:20:04 +0200

    alphabts file add

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 'a';
+while(i <= 'z')
+{
+if (i = 'q' || 'e')
+{
+
+}
+else{
+putchar(i);
+i++;
+}
+}
+putchar('\n');
+return (0);
+}
