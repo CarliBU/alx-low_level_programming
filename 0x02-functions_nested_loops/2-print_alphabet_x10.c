c543d7ac33705b4b1fd016f3b2bf27f3763a222d
Carli <carlibuckle@gmail.com>
2022-03-15 20:08:35 +0200

    check

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Entry point
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
