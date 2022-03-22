c0386067b32810e63ca16d391d82a7f6929e1300
CarliBU <carlibuckle@gmail.com>
2022-03-22 20:05:45 +0200

    add puts file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+/**
+*_puts - prints a string + newline to standard output
+*
+*@str: string to be printed
+*
+*Return: void
+*/
+void _puts(char *str)
+{
+char string = str;
+while (*string != '\0')
+{
+putchar(*string);
+s++;
+}
+putchar('\n');
+}
