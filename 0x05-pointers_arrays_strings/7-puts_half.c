a320a6d9b0185fe7ebcdd492f987504c1fade9bc
CarliBU <carlibuckle@gmail.com>
2022-03-22 21:45:51 +0200

    puts half file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - prints second half of string followed by new line
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int str_size;
+
+	str_size = strlen(str);
+	i = str_size / 2;
+	if (str_size % 2 != 0)
+		i++;
+
+	for (i = 0; i < str_size; i+=2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
