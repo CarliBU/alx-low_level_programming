d803a8cdee5c76b5e0e86e66eb8c7149a3ce7b50
CarliBU <carlibuckle@gmail.com>
2022-03-22 19:25:26 +0200

    add reset to 98 file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+*reset_to_98 - takes a pointer to an int as paramterer and updates
+*value it points to to 98
+*
+*@n: poinmter to value to be reset
+*
+*Return: void
+*/
+void reset_to_98(int *n)
+{
+*n = 98
+}
