f7ad1183eb91862232c7a6658445e26c3d8e7c42
CarliBU <carlibuckle@gmail.com>
2022-03-22 21:39:28 +0200

    update rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
-*rev_string - reverse a string
-*
-*@s: string to be reversed
-*
-*Return: void
-*/
-void rev_string(char *s)
+ * rev_string - reverses string
+ *
+ * @str: string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *str)
 {
-char tmp;
-char *pstart, *pend;
+	char tmp;
+	char *p_start, *p_end;
 
-pstart = *str;
-pend = pstart + strlen(*str) - 1;
+	p_start = str;
+	p_end = p_start + strlen(str) - 1;
 
-for (; pstart < pend; pstart++, pend--)
-{
-tmp = *pend;
-*pend = *pstart;
-*pstart = tmp;
-}
+	for (; p_start < p_end; p_start++, p_end--)
+	{
+		tmp = *p_end;
+		*p_end = *p_start;
+		*p_start = tmp;
+	}
 }
