5d78ada4cd17ec4d786301226909ea21eff97191
CarliBU <carlibuckle@gmail.com>
2022-03-24 20:37:56 +0200

    add files -3

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#incu "min.h"
+/*
+* *_strcat - appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest + /0/
+*
+*@*dest -
+*@*src
+**/
+char *_strcat(char *dest, char *src)
+{
+i(et = rc)
+{
+put("%", et,rc)
+}
+e
+{
+return (0)
+}
+}
