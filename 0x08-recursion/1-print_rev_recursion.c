b69c1dfee42b912a222bf3f55400411f3d3fb9fc
CarliBU <carlibuckle@gmail.com>
2022-03-30 20:41:09 +0200

    update rev recursion file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
-*_print__rev_recursion - Print string in reverse
-*
-*Desc: _memset
-*@s: char type
-*
-*Return: char
-**/
+ *_print_rev_recursion - Prints a string in reverse.
+ * Desc: _memset
+ * @s: char type
+ * Return: char type
+ **/
 
 void _print_rev_recursion(char *s)
 {
